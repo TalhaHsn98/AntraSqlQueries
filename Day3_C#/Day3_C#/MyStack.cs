71a7c43c5255ee70757e24abc5b004593420005e
TalhaHsn98 <syedtalha.hussain98@gmail.com>
2025-08-09 00:07:38 -0400

    Day 3 Homework

---
+++ Day3_C#/Day3_C#/MyStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_C_
+{
+    public class MyStack<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public int Count()
+        {
+            return _items.Count;
+        }
+
+        public void Push(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T Pop()
+        {
+
+            var lastIndex = _items.Count - 1;
+            T item = _items[lastIndex];
+            _items.RemoveAt(lastIndex);
+            return item;
+        }
+    }
+}
---
+++ Day3_C#/Day3_C#/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day3_C_;
+
+Console.WriteLine("Hello, World!");
+
+
+//Stack
+
+MyStack<int> stack = new MyStack<int>();
+
+stack.Push(10);
+stack.Push(20);
+stack.Push(30);
+
+Console.WriteLine("Count: " + stack.Count()); // 3
+
+Console.WriteLine("Popped: " + stack.Pop());  // 30
+Console.WriteLine("Count: " + stack.Count()); // 2
